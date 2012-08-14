27a7550f37533cbb56f10f3d678473e68b61d930
Benson Wong <mostlygeek@gmail.com>
2012-08-14 11:01:30 -0700

    add comments to scala code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,20 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
+/**
+ * This case class holds the json message from the client. 
+ *
+ * In order to convert a blob of json into a case class
+ * we need a custom reader/writer for it. The conversion by 
+ * Play's JSON library is done implicitly. 
+ *
+ * This is the pattern recommended in the docs. Create a
+ * case class and then manually create reads/writes functions
+ * to convert it.
+ *
+ * ... it would be nice to use some reflections here to 
+ * automatically do simple conversions... 
+ */
 case class Message(sender: String, message: String)
 object Message {
   implicit object JsonReaderWriter extends Format[Message] {
@@ -24,20 +38,27 @@ object Application extends Controller {
     Ok(views.html.index())
   }
  
+  /**
+   * the endPoint for the /echoName JSON ajax call 
+   */
   def echoName = Action(parse.json) { request => 
     (request.body \ "name").asOpt[String].map { name => 
       val json = Json.toJson(Map("name" -> name))
       Ok(json)
     }.getOrElse {
-      BadRequest("Missing json value: [name]")
+      BadRequest(Json.toJson(Map("err" -> "missing [name] field")))
     }
   }
 
+  /**
+   * the end point for the /echoMsg JSON ajax call
+   */
   def echoMsg = Action(parse.json) { request =>
+    // notice the asOpt[Message] ... automatic conversion to a case class..
     request.body.asOpt[Message].map { msg =>
       Ok(Json.toJson(Message("Play Framework", "echo: " + msg.message)))
     }.getOrElse {
-      BadRequest("boom")
+      BadRequest(Json.toJson(Map("err" -> "invalid message format")))
     }
   }
   
